a0446150981c5a82d20e03f7cba7799d6be37986
Juan Bermudez <114368367+juanbermudezg@users.noreply.github.com>
2024-02-28 15:26:45 -0500

    Add project java crud

---
+++ Java_crud_mysql/src/main/java/com/juanbermudez/java_crud_mysql/CAlumnos.java
@@ -0,0 +1,39 @@
+package com.juanbermudez.java_crud_mysql;
+/**
+ * @author juanbermudezg
+ */
+public class CAlumnos {
+    
+    private int id;
+    private String nombreAlumno;
+    private String apellidoAlumno;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombreAlumno() {
+        return nombreAlumno;
+    }
+
+    public void setNombreAlumno(String nombreAlumno) {
+        this.nombreAlumno = nombreAlumno;
+    }
+
+    public String getApellidoAlumno() {
+        return apellidoAlumno;
+    }
+
+    public void setApellidoAlumno(String apellidoAlumno) {
+        this.apellidoAlumno = apellidoAlumno;
+    }
+    
+    public void insetarAlumno(){
+        
+    }
+    
+}
